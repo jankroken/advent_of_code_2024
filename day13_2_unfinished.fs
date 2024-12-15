7915a1e93bc975419ef5cd38778f60bb01677611
Jan Kroken <jan.kroken@gmail.com>
2024-12-15 05:52:33 +0100

    day 13.2 and 14 (not finished)

---
+++ day13_2_unfinished.fs
@@ -0,0 +1,102 @@
+﻿open System.IO
+
+let print a = a |> printfn "%A"
+let printlist a = List.map print
+let inputFile = "/Users/jan/downloads/input-nav.txt"
+let input = File.ReadAllLines inputFile |> Array.toList
+
+type Button = int64 * int64
+type Prize = int64 * int64
+type Machine = Button * Button * Prize
+
+let rec gcd x y = if y = 0L then x else gcd y (x % y)
+
+let gcd2 (x1, y1) (x2, y2) =
+    let gx = gcd x1 x2
+    let gy = gcd y1 y2
+    gcd gx gy
+
+let lowestOf (a: Option<int64>, b: Option<int64>) =
+    match a, b with
+    | None, None -> None
+    | None, Some(c) -> Some(c)
+    | Some(c), None -> Some(c)
+    | Some(a), Some(b) -> Some(min a b)
+
+
+let split (l: string list) =
+    let rec split (acc: string list list) (l: string list) =
+        match (acc, l) with
+        | acc, [] -> acc |> List.map List.rev |> List.rev
+        | acc, i :: rest when i = "" -> split ([] :: acc) rest
+        | [], i :: rest -> split [ [ i ] ] rest
+        | a :: acc, i :: rest -> split ((i :: a) :: acc) rest
+
+    split [] l
+
+let blocks = split input
+
+let parseBlock (block: string list) : Machine =
+    let a = block[0].Split("+") |> Array.tail
+    let a = a[0].Split(",") |> Array.head |> int64, a[1] |> int64
+    let b = block[1].Split("+") |> Array.tail
+    let b = b[0].Split(",") |> Array.head |> int64, b[1] |> int64
+    let prize = block[2].Split("=") |> Array.tail
+    let prize = prize[0].Split(",") |> Array.head |> int64, prize[1] |> int64
+    a, b, prize
+
+let machines = blocks |> List.map parseBlock
+
+machines |> List.map (printfn "MACHINE: %A")
+
+let adjustment = 10000000000000L
+
+let machines2 =
+    machines
+    |> List.map (fun (a, b, (px, py)) -> a, b, (px + adjustment, py + adjustment))
+
+
+let gcd3 (a:int64, b:int64, c:int64) =
+    let ab = gcd a b
+    let ac = gcd a c
+    let abc = gcd ab ac 
+    printfn $"ab={ab} ac={ac} abc={abc}"
+    abc
+    
+let shrink_by_gcd3 (a,b,c) =
+    let div = gcd3 (a,b,c)
+    (a/div,b/div,c/div)
+
+let solve (limit:bool) (ac:int64,bc:int64) (machine:Machine) =
+    let ((x1,y1),(x2,y2),(xp,yp)) = machine
+    let b = if (x1*y1-y2*x1) = 0 then None
+            else Some ((xp*y1-yp*x1)/(x2*y1-y2*x1))
+    let a =
+        if b = None then None
+        elif (x1-y1) = 0 then None
+        else 
+        Some (((b.Value*(y2-x2)) + (xp-yp)) / (x1-y1))
+ 
+    if a.IsNone then None
+    elif limit && (a.Value > 100L || b.Value > 100L) then None 
+    else 
+        let success = a.Value*x1 + b.Value*x2 = xp 
+        printfn $"a={a.Value} b={b.Value} tokens={a.Value*3L+b.Value} success={success}"
+        if success then Some (a.Value*ac+b.Value*bc) else None 
+    
+// solve2 ((94L,34L),(22L,67L),(8400L,5400L))
+
+let swap (a,b) = (b,a)
+
+let solve1 (a,b,c) =
+    let s1 = solve true (3,1) (a,b,c)
+    let s2 = solve true (1,3) (swap b,swap a,swap c)
+    printfn $"SOLUTIONS {s1} {s2}"
+    match s1,s2 with
+    | None,None -> None
+    | Some(s1),Some(s2) -> Some(min s1 s2)
+    | _ ->
+        printfn $"****** WEIRD ***** {s1},{s2}"
+        None
+    
+let result1 = machines |> List.map solve1 |> List.choose id |> List.sum |> printfn "ANSWER 1: %A"
---
+++ day14_unfinished.fs
@@ -0,0 +1,168 @@
+﻿open System.IO
+
+let print a = a |> printfn "%A"
+let printlist a = a |> List.map print
+let printlistf f a = a |> List.map (printfn f)
+
+let test = false
+
+let inputFile =
+    if test then
+        "input-test.txt"
+    else
+        "input.txt"
+
+let width = if test then 11 else 101
+let height = if test then 7 else 103
+let input = File.ReadAllLines inputFile |> Array.toList
+
+let middleX = (width - 1) / 2
+
+input |> printlist
+
+type Pos = int * int
+type Vel = int * int
+type Robot = Pos * Vel
+
+let parse (s: string) : Robot =
+    let s = s.Split(" ")
+    let pos = s[0].Split("=")
+    let pos = pos[1].Split(",")
+    let pos = (pos[0] |> int, pos[1] |> int)
+    let vel = (s[1].Split("=")[1]).Split(",")
+    let vel = (vel[0] |> int, vel[1] |> int)
+    pos, vel
+
+let robots = input |> List.map parse
+
+robots |> printlistf "ROBOT %A"
+
+let moverobot ((((x, y): Pos), ((dx, dy): Vel)): Robot) : Robot =
+    let (x, y), vel = (x + dx, y + dy), (dx, dy)
+    let x = (x + width) % width
+    let y = (y + height) % height
+    (x, y), vel
+
+let tick (robots: Robot list) : Robot list = robots |> List.map moverobot
+
+let rec tickn (n: int) (robots: Robot list) : Robot list =
+    if n = 0 then robots else robots |> tick |> tickn (n - 1)
+
+let robots100 = robots |> tickn 100
+
+let quadrant1 =
+    robots100
+    |> List.filter (fun ((x, y), _) -> x < ((width - 1) / 2) && y < ((height - 1) / 2))
+
+let quadrant2 =
+    robots100
+    |> List.filter (fun ((x, y), _) -> x > ((width - 1) / 2) && y < ((height - 1) / 2))
+
+let quadrant3 =
+    robots100
+    |> List.filter (fun ((x, y), _) -> x < ((width - 1) / 2) && y > ((height - 1) / 2))
+
+let quadrant4 =
+    robots100
+    |> List.filter (fun ((x, y), _) -> x > ((width - 1) / 2) && y > ((height - 1) / 2))
+
+let safe (quad: Robot list) = quad |> List.length
+
+let quads =
+    [ quadrant1; quadrant2; quadrant3; quadrant4 ]
+    |> List.map safe
+    |> List.map int64
+
+quads |> printfn "%A"
+let safety = quads[0] * quads[1] * quads[2] * quads[3]
+
+printfn $"ANSWER 1 {safety}"
+
+let example: Robot = (2, 4), (2, -3)
+
+let rec movern (n: int) (robot: Robot) =
+    if n = 0 then
+        true
+    else
+        printfn $"ROBOT @ {n} : {robot}"
+        moverobot robot |> movern (n - 1)
+
+// movern 6 example
+
+let printrobots (robots: Set<Pos>) =
+    let xs = seq { 0 .. width - 1 } |> Seq.toList
+    let ys = seq { 0 .. height - 1 } |> Seq.toList
+
+    let charAt (pos) =
+        if robots.Contains pos then "#" else "."
+
+    let toString y =
+        let chars = xs |> List.map (fun x -> charAt (x, y)) |> List.toArray
+        System.String.Concat(chars)
+
+    ys |> List.map toString |> List.map (printfn "%s")
+
+let wrap (x, y) =
+    if x > (width - 1) / 2 then (width - x - 1, y) else (x, y)
+
+let possibleTree (robots: Robot list) =
+    let robots = robots |> List.map fst |> Set.ofList
+    let midX = (width - 1)
+
+    if robots.Contains (midX, 0) |> not then
+        false
+    else
+        let nostem = robots |> Set.filter (fun (x, y) -> x <> (width - 1) / 2)
+        let wrapped = nostem |> Set.map wrap
+
+        if wrapped.Count * 2 <> nostem.Count then
+            false
+        else
+            printfn "Possible:"
+            printrobots robots
+            true
+
+wrap (50, 0) |> printfn "WRAPPED: %A"
+(width - 1) / 2 |> printfn "MID: %A"
+
+let robots2 =
+    [ ((50, 0), (0, 0)); ((49, 1), (0, 0)); ((50, 1), (0, 0)); ((51, 1), (0, 0)) ]
+
+// possibleTree robots2
+
+let quickCheck1 (robots:Robot list) =
+    let first = robots |> List.minBy (fun ((x,y),_) -> (y,x) |> snd)
+    // printfn $"min: {first}"
+    let ((top_x, _),_) = first
+    top_x = middleX
+
+let rec findTree (count: int) (robots: Robot list) =
+    if count % 100000 = 0 then
+        printfn $"@{count}"
+        // robots |> List.map fst |> Set.ofList |> printrobots
+        ()
+    if quickCheck1 robots then
+        printfn $"POSSIBLE: {count}"
+        robots |> List.map fst |> Set.ofList |> printrobots
+    else
+        let robots = robots |> List.map moverobot
+        let limit = count + 1
+        findTree limit robots
+
+findTree 0 robots
+
+
+let findPos((tx,ty):Pos) (((x,y),(dx,dy)): Robot) : int64*int64 =
+    printfn $"robot: {tx},{ty} <- {x},{y} , {dx},{dy}"
+    
+    (0,0)
+    
+    
+
+findPos (0,50) robots[1]
+
+robots |> List.filter (fun (_,(a,b)) -> a = 0 or b = 0 ) |> printlist 
+
+
+robots.Length |> print
+robots |> List.max |> print 
